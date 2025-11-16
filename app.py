2614980f9dab668b138b6374ea7199ce0ed958f3
Deathstriker <shoaibfarooq122@gmail.com>
2025-11-16 12:55:28 +0500

    files set

---
+++ app.py
@@ -0,0 +1,10 @@
+﻿from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return send_file('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
